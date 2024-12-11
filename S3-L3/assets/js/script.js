99d2fc4b062d97d286d767ee3963b8f92b71ff4a
RosarioFaro <farorosario134@gmail.com>
2024-12-11 16:24:17 +0100

    S3-L3

---
+++ S3-L3/assets/js/script.js
@@ -0,0 +1,41 @@
+let data = [];
+
+function aggData() {
+    let dataInput = document.querySelector('#input');
+
+    let obj = {
+        testo: dataInput.value
+    };
+
+    data.push(obj);
+    resetForm(dataInput);
+    dataInputList();
+}
+
+function resetForm(dataInput) {
+    dataInput.value = '';
+}
+
+function dataInputList() {
+    let dataList = document.querySelector('section');
+    dataList.innerHTML = '';
+
+    let listaUn = document.createElement('ul');
+    data.forEach((ele, i) => {
+        let li = document.createElement('li');
+        li.innerHTML = `${ele.testo} <span><button onclick="deleteData(${i})"><i class="fas fa-times-circle"></i></button></span>`;
+        
+        li.addEventListener('click', () => {
+            li.style.textDecoration = li.style.textDecoration === 'line-through' ? 'none' : 'line-through';
+        });
+
+        listaUn.appendChild(li);
+    });
+
+    dataList.appendChild(listaUn);
+}
+
+function deleteData(i) {
+    data.splice(i, 1)
+    dataInputList()
+}
